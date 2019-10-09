590b024dfdb8b56550dc66e2f9e389c98efe656a
ghkdtlsvlf <56142400+ghkdtlsvlf@users.noreply.github.com>
2019-10-09 12:46:16 +0900

    Drill-04-1
    Drill

--- character_runs.py
+++ character_runs.py
@@ -1,21 +1,39 @@
 from pico2d import *
+
 open_canvas()
 grass = load_image('grass.png')
-character = load_image('run_animation.png')
+character = load_image('animation_sheet.png')
 
-x = 0
 
-frame = 0
-while x < 800:
-    clear_canvas()
-    grass.draw(400, 30)
-    character.clip_draw(frame*100, 0, 100, 100, x, 90)
-    update_canvas()
-    frame = (frame + 1) % 8
-    x += 5
-    delay(0.05)
+def Move_left():
+    x = 0
+    frame = 0
+    while x < 800:
+        clear_canvas()
+        grass.draw(400, 30)
+        character.clip_draw(frame * 100, 100, 100, 100, x, 90)
+        update_canvas()
+        frame = (frame + 1) % 8
+        x += 5
+        delay(0.05)
+
+
+def Move_Right():
+    x = 800
+    frame = 0
+    while x > 0:
+        clear_canvas()
+        grass.draw(400, 30)
+        character.clip_draw(frame * 100, 0, 100, 100, x, 90)
+        update_canvas()
+        frame = (frame + 1) % 8
+        x -= 5
+        delay(0.05)
+
+
+while True:
+    Move_left()
+    Move_Right()
     get_events()
 
-
 close_canvas()
-
